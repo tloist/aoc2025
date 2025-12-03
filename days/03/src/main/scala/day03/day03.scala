460f931a26e7a51ccf9f5af30653c86867d57781
Timo Loist <timo.loist@innoq.com>
2025-12-03 10:18:23 +0100

    Day03 -- Make it tail recursive (Performance tweak, not necessary for this input)

--- days/03/src/main/scala/day03/day03.scala
+++ days/03/src/main/scala/day03/day03.scala
@@ -1,19 +1,22 @@
 package day03
 
+import scala.annotation.tailrec
+
 case class Bank(batteries: List[Int])
 def parseInput(filename: String = "input.txt"): List[Bank] =
   os.read.lines(os.resource / filename).toList.map: line =>
     Bank(line.map(_.asDigit).toList)
 
-def findMaxJoltage(batteries: List[Int], size: Int): String =
-  if size <= 1 then batteries.max.toString
+@tailrec
+def findMaxJoltage(res: String, batteries: List[Int], size: Int): String =
+  if size <= 1 then res + batteries.max.toString
   else
     val minimumRemainingElements = size - 1
     val max = batteries.slice(0, batteries.length - minimumRemainingElements).max
     val idx = batteries.indexOf(max)
-    max.toString + findMaxJoltage(batteries.slice(idx + 1, batteries.length), size - 1)
-def findMaxJoltage_2(bank: Bank): BigInt = BigInt(findMaxJoltage(bank.batteries, 2))
-def findMaxJoltage_12(bank: Bank): BigInt = BigInt(findMaxJoltage(bank.batteries, 12))
+    findMaxJoltage(res + max.toString, batteries.slice(idx + 1, batteries.length), size - 1)
+def findMaxJoltage_2(bank: Bank): BigInt = BigInt(findMaxJoltage("", bank.batteries, 2))
+def findMaxJoltage_12(bank: Bank): BigInt = BigInt(findMaxJoltage("", bank.batteries, 12))
 
 @main def total_output_joltage(): Unit =
   val totalJoltageSum = parseInput().map(findMaxJoltage_2).sum
